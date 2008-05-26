985b891269f05dfb8f13f0201a41f88e71f0a5d2
Marcus Crafter <crafterm@redartisan.com>
2008-05-27 09:25:23 +1000

    Added brief information and usage text

--- color-theme-twilight.el
+++ color-theme-twilight.el
@@ -1,3 +1,23 @@
+;; Twilight Colour Theme for Emacs.
+;;
+;; Defines a colour scheme resembling that of the original TextMate Twilight colour theme.
+;; To use add the following to your .emacs file (requires the color-theme package):
+;;
+;; (require 'color-theme)
+;; (color-theme-initialize)
+;; (load-file "~/.emacs.d/twilight-emacs/color-theme-twilight.el")
+;;
+;; And then (color-theme-twilight) to activate it.
+;;
+;; Several areas still require improvement such as recognition of code that ruby-mode doesn't
+;; yet pick up (eg. parent classes), Rails/Merb keywords, or non Ruby code related areas
+;; (eg. dired, HTML, etc). Please feel free to customize further and send in any improvements,
+;; patches most welcome.
+;;
+;; MIT License Copyright (c) 2008 Marcus Crafter <crafterm@redartisan.com>
+;; Credits due to the excellent TextMate Twilight theme
+;;
+
 (defun color-theme-twilight ()
   "Color theme by Marcus Crafter, based off the TextMate Twilight theme, created 2008-04-18"
   (interactive)
